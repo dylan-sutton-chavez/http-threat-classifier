c0493c8c4619a85f4b125984700d77053167d702
Dylan Sutton Chavez <c.sutton.dylan@gmail.com>
2025-10-27 22:38:08 -0600

    Update url_analysis.py

--- http_request_vectorizer/url_analysis.py
+++ http_request_vectorizer/url_analysis.py
@@ -4,9 +4,21 @@ class UrlAnalyisis:
     def __init__(self, raw_url: str):
         self.url_length: int = len(raw_url) # calculate the length of the url
 
-        url_path_splited = self._split_url_path(raw_url)
+        self.url_path_splited: list[str] = self._split_url_path(raw_url)
 
-        self.path_depth: int = len(url_path_splited)
+        self.path_depth: int = len(self.url_path_splited)
+        
+
+        last_path: str =  self.url_path_splited[-1]
+
+        if '?' in last_path:
+            for idx, char in enumerate(last_path):
+                if char == '?':
+                    self.query: str | list[float] = last_path[idx:]
+                    break
+                    
+        else:
+            self.query: str = None
 
     def _split_url_path(self, raw_url: str):
         path_only = sub(r'^(?:\w+://)?[^/]+', '', raw_url)

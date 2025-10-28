a8f2dfe485855a645f6fe2a4a49fd285903e9fbf
Dylan Sutton Chavez <c.sutton.dylan@gmail.com>
2025-10-27 23:00:39 -0600

    Update url_analysis.py

--- http_request_vectorizer/url_analysis.py
+++ http_request_vectorizer/url_analysis.py
@@ -2,27 +2,49 @@ from re import sub
 
 class UrlAnalyisis:
     def __init__(self, raw_url: str):
+        """
+        initialize the url analysis object and calculate the stadistics
+        
+        args:
+            raw_url: → receive a raw url
+
+        output:
+            None
+
+        time complexity → o(n)
+        """
         self.url_length: int = len(raw_url) # calculate the length of the url
-
         self.url_path_splited: list[str] = self._split_url_path(raw_url)
-
         self.path_depth: int = len(self.url_path_splited)
         
+        # check if the path list is not empty
+        if self.url_path_splited:
+            last_path: str =  self.url_path_splited[-1] # save the last path element
+            # find if the path have a query 
+            if '?' in last_path:
+                # iterate for each last path element
+                for idx, char in enumerate(last_path):
+                    # check if the current index is a query
+                    if char == '?':
+                        self.query: str | list[float] = last_path[idx:]
+                        self.url_path_splited[-1] = last_path[:idx]
+                        break
 
-        last_path: str =  self.url_path_splited[-1]
-
-        if '?' in last_path:
-            for idx, char in enumerate(last_path):
-                if char == '?':
-                    self.query: str | list[float] = last_path[idx:]
-
-                    self.url_path_splited[-1] = last_path[:idx]
-                    break
-
-        else:
-            self.query: str = None
+            else:
+                self.query: str = None
 
     def _split_url_path(self, raw_url: str):
+        """
+        split a raw url into a paths list
+        
+        args:
+            raw_url: → receive a raw url
+
+        output:
+            list[str] → return a list with the paths
+
+        time complexity → o(n)
+        """
         path_only = sub(r'^(?:\w+://)?[^/]+', '', raw_url)
         stripped_path = path_only.strip('/')
         
@@ -31,6 +53,15 @@ class UrlAnalyisis:
 if __name__ == '__main__':
     """run this block when the code is runed directly"""
 
-    url_analysis = UrlAnalyisis('https://www.ibm.com/cloud/learn/what-is-hybrid-cloud')
+    # create a deept analysis of the url
+    url_analysis = UrlAnalyisis('https://www.ibm.com/cloud/learn/what-is-hybrid-cloud/user?=id1245')
 
-    print(url_analysis.path_depth)
+    # structure the object atributes
+    url_analysis_dict = {
+        'url_length': url_analysis.url_length,
+        'url_path_splited': url_analysis.url_path_splited,
+        'path_depth': url_analysis.path_depth,
+        'query': url_analysis.query
+    }
+
+    print(url_analysis_dict)

df57fa8495ab6c6817b5aed72d6d07471ff05f16
Dylan Sutton Chavez <c.sutton.dylan@gmail.com>
2025-10-27 22:08:14 -0600

    Create header.py

---
+++ http_request_vectorizer/header.py
@@ -0,0 +1,9 @@
+class Header:
+    def __init__(self):
+        self.user_agent: str = None
+        self.referer: str = None
+        self.accept_language: str = None
+        self.content_type: str = None
+        self.host: str = None
+        self.cookie: str = None
+        self.authorization: str = None

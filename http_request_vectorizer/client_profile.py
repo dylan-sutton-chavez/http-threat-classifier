67280955cf9e11bed6b680b43b9ff9ee8ea44891
Dylan Sutton Chavez <c.sutton.dylan@gmail.com>
2025-10-28 22:49:41 -0600

    Add files via upload

--- http_request_vectorizer/client_profile.py
+++ http_request_vectorizer/client_profile.py
@@ -1,9 +1,38 @@
 class ClientProfile:
-    def __init__(self):
+    def __init__(self, method: str):
         self.origin_ip: str = None
         self.geolocalization: str = None
-        self.http_method: str = None
+        self.http_method: float = self._method_encoder(method)
         self.request_rate: int = None
         self.errors_rate: int = None
         self.device_type: int = None
         self.browser: int = None
+
+    def _method_encoder(method: str):
+        """
+        encode the method in a floatant value
+
+        args:
+            method: str → method of the user
+
+        output:
+            float → encoded value of the method
+
+        time complexity → o(n)
+        """
+        method_lower: str = method.lower()
+
+        actions_bag = {
+            "get": 1.0,
+            "head": 1.0,
+            "post": 1.0,
+            "put": 0.5,
+            "patch": 0.5,
+            "delete": 0.5,
+            "options": 0.5,
+        }
+
+        if method_lower in actions_bag:
+            return actions_bag[method_lower]
+        
+        return 0.0
